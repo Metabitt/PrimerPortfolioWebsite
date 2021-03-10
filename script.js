aca112341b6ab0cf2bea87520fa595e76d6b8ac0
Metabitt <angelipanake@gmail.com>
2021-03-10 18:51:18 -0500

    BASE PORTFOLIO COMPLETED
    Base for the portfolio 100% funtional

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
 
+    //typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Portfolio", "Portafolio", "CV", "Miau"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["self-taught Programer", "Engineer", "Gadget-man", "Cat-lover"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 });
